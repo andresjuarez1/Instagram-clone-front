19fcad1468b9dc233ff92d351cad89e89598309b
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-03-17 11:22:35 -0600

    TailwindCSS and PostCSS

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = ()=>{
+
+    return(
+        <>
+            <div>
+                <h1 className="text-6xl uppercase ">Instagram clone</h1>
+            </div>
+        </>
+    );
+
+};
+
+export default Header;
